00b03a5121f69cbfbf702ecf47024f90a5cfdc3c
Annelise Cope <65578617+anniid@users.noreply.github.com>
2020-10-18 14:45:34 -0500

    trying tooltips a different way, bugfixing

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 100, bottom: 40, left: 60 },
+var margin = { top: 10, right: 100, bottom: 30, left: 60 },
   width = 1000 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
@@ -17,6 +17,7 @@ var svg = d3.select("#scatter")
   .append("g")
   .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
+    
 
 //Read the data
 d3.csv("data/winning_ratio.csv", function (data) {
@@ -40,14 +41,6 @@ d3.csv("data/winning_ratio.csv", function (data) {
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
-  
-    //axis label
-  svg.append('text')
-  .attr('x', width - 360)
-  .attr('y', height +35)
-  .attr('text-anchor', 'end')
-  .attr('class', 'label')
-  .text('Selected Attribute');
 
   // Add Y axis
   var y = d3
@@ -57,46 +50,22 @@ d3.csv("data/winning_ratio.csv", function (data) {
 
   svg.append("g")
     .call(d3.axisLeft(y));
-  
-yText = 'Winning Rate (%)';
-    //axis label
-  svg.append('text')
-  .attr("transform", "translate(-290, 235)rotate(-90)")
-		.attr('x', -25)
-		.attr('y', 255)
-		.attr('class', 'label')
-    .text('Win Rate (%)');
 
 //custom colorscale
     var colorscale = d3.scaleOrdinal() // D3 Version 4
     .domain(["Normal", "Fire", "Water", "Electric","Grass","Ice", "Fighting","Poison","Ground","Flying","Psychic","Bug","Rock","Ghost","Dragon","Dark","Steel", "Fairy"])
     .range(["#A8A77A", "#EE8130","#6390F0","#F7D02C","#7AC74C","#96D9D6", "#C22E28", "#A33EA1", "#E2BF65", "#A98FF3", "#F95587", "#A6B91A", "#B6A136", "#735797", "#6F35FC", "#705746","#B7B7CE", "#D685AD"]);
 
+   
   // Tooltip instantiate
-  var tooltip = d3.select("#scatter").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0);
-
-  // tooltip mouseover event handler
-  var tipMouseover = function(d) {
-    var color = colorscale(d.Type_1);
-    var html  ="<b>" + d.Name + ", #" + d.Num + "<br> Primary Type: " + "<span style='color:" + color + ";'>" + d.Type_1 + "</span></br></b>";
-
-      tooltip.html(html)
-          .style("left", (d3.event.pageX + 15) + "px")
-          .style("top", (d3.event.pageY - 28) + "px")
-        .transition()
-          .duration(200) // ms
-          .style("opacity", .9) // started as 0!
-
-  };
-  // tooltip mouseout event handler
-  var tipMouseout = function(d) {
-      tooltip.transition()
-          .duration(300) // ms
-          .style("opacity", 0); // don't care about position!
-  };
-
+  var tip = d3.tip()
+  .attr("class", "d3-tip")
+  .offset([-10, 0])
+  .html(function(d) {
+      return "<b>" + d.Name + ", #" + d.Num + "<br> Primary Type: " + "<span style='color:" + colorscale(d.Type_1) + ";'>" + d.Type_1 + "</span></br></b>";
+  });
+  svg.call(tip);
+  
   // Initialize dots with first attribute
   var dot = svg
     .selectAll('circle')
@@ -107,9 +76,8 @@ yText = 'Winning Rate (%)';
     .attr("cy", function (d) { return y(+d.Winning_Perc) })
     .attr("r", 7)
     .style("fill", function(d) { return colorscale(d.Type_1); })
-      .on("mouseover", tipMouseover)
-      .on("mouseout", tipMouseout);
-      
+      .on("mouseover", tip.show)
+      .on("mouseout", tip.hide)
   //legend
   var clicked = ""
   var legend = svg.selectAll(".legend")
@@ -120,36 +88,43 @@ yText = 'Winning Rate (%)';
           return "translate(0," + i * 20 + ")";
       });
   legend.append("rect")
-      .attr("x", width)
+      .attr("x", width + 10)
       .attr("width", 12)
       .attr("height", 12)
       .style("fill", function(d) { return colorscale(d); });
 
-  legend.on("click", function(type) {
-      // select all dots and apply 0 opacity (hide)
-      d3.selectAll(".dot").style("opacity", 1)
-     
-      // filter out the ones we want to show and apply properties
-
-    if (clicked !== d){
-      d3.selectAll(".dot")
-        .filter(function(e){
-        return e.Type_1 !== d;
-      })
-        .style("opacity",0.1)
-      clicked = d
-    }
-      else{
-        clicked = ""
-      }
+      legend.on("click", function(type) {
+        // dim all of the icons in legend
+        d3.selectAll(".legend")
+            .style("opacity", 0.1);
+        // make the one selected be un-dimmed
+        d3.select(this)
+            .style("opacity", 1);
+        // select all dots and apply 0 opacity (hide)
+        d3.selectAll(".dot")
+        // .transition()
+        // .duration(500)
+        .style("opacity", 0.0)
+        // filter out the ones we want to show and apply properties
+        .filter(function(d) {
+            return d["Type_1"] == type;
+        })
+        .style("opacity", 1) // need this line to unhide dots
+        .style("stroke", "black")
+        // apply stroke rule
+        .style("fill", function(d) {
+            if (d.Type_1 == type) {
+                return this
+            } else {
+                return "white"
+            };
+        });
     });
 
     legend.append("text")
         .attr("x", width + 25)
         .attr("dy", ".65em")
         .text(function(d) { return d; });
-
-
 //A function that updates the chart
 function update(selectedGroup) {
 
